dcfd77a8a6ab75e1b5d67a5579fb696efe33449d
Matthew Bilyeu <matt@matthewbilyeu.com>
2024-06-04 22:09:51 -0400

    button stuff

--- timerWorker.js
+++ timerWorker.js
@@ -3,44 +3,40 @@ let breakTime = 0;
 let isWorking = true;
 let timer;
 
-onmessage = function(e) {
-    const command = e.data.command;
-    
-    if (command === 'startWork') {
-        clearInterval(timer);
-        isWorking = true;
-        workTime = e.data.workTime;
-        timer = setInterval(() => {
-            workTime++;
-            breakTime = Math.floor(workTime / 3);
-            postMessage({ workTime, breakTime });
-        }, 1000);
-    }
+onmessage = function (e) {
+  const command = e.data.command;
 
-    if (command === 'startBreak') {
-        clearInterval(timer);
-        isWorking = false;
-        breakTime = e.data.breakTime;
-        workTime = 0;
+  if (command === "startWork") {
+    clearInterval(timer);
+    isWorking = true;
+    workTime = e.data.workTime;
+    timer = setInterval(() => {
+      workTime++;
+      breakTime = Math.floor(workTime / 3);
+      postMessage({ workTime, breakTime });
+    }, 1000);
+  }
+
+  if (command === "startBreak") {
+    clearInterval(timer);
+    isWorking = false;
+    breakTime = e.data.breakTime;
+    workTime = 0;
+    postMessage({ workTime, breakTime });
+    timer = setInterval(() => {
+      breakTime--;
+      if (breakTime === 0) {
+        postMessage({ breakTime, alert: true });
+      } else {
         postMessage({ workTime, breakTime });
-        timer = setInterval(() => {
-            breakTime--;
-            if (breakTime === 0) {
-                postMessage({ breakTime, alert: true });
-            } else {
-                postMessage({ workTime, breakTime });
-            }
-        }, 1000);
-    }
+      }
+    }, 1000);
+  }
 
-    if (command === 'pause') {
-        clearInterval(timer);
-    }
-
-    if (command === 'reset') {
-        clearInterval(timer);
-        workTime = 0;
-        breakTime = 0;
-        postMessage({ workTime, breakTime });
-    }
+  if (command === "stop") {
+    clearInterval(timer);
+    workTime = 0;
+    breakTime = 0;
+    postMessage({ workTime, breakTime });
+  }
 };
